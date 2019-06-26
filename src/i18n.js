58dca87c1c94bdfefb65600f1493d248616217ac
Hans Jerry Illikainen <hji@dyntopia.com>
2019-06-26 10:55:40 +0000

    i18n: setup internationalization

---
+++ src/i18n.js
@@ -0,0 +1,16 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+import { locales } from './locales';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: locales,
+    lng: 'en',
+    fallbackLng: 'en',
+  });
+
+export default i18n;
---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+const en = {
+  translation: {
+    label: {
+      username: 'Username',
+      password: 'Password',
+      login: 'Login',
+      home: 'Home',
+    },
+    message: {
+      invalidCredentials: 'Invalid credentials',
+    },
+  },
+};
+
+export { en };
