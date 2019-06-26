3580a742d40fa72d1fcd2a353e21bd04b9d67647
Hans Jerry Illikainen <hji@dyntopia.com>
2019-06-26 11:28:21 +0000

    components: update App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import React, { useState } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import '../i18n';
+import { Navigation } from './Navigation';
+import { TopBar } from './TopBar';
+import { Conditional } from './Conditional';
+import { Routes } from './Routes';
+import { Content } from './Content';
 
 const App = () => {
-  return <p>hi!</p>;
+  const [user, setUser] = useState(window.user || {});
+  const { t } = useTranslation();
+
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <Box display="flex">
+        <TopBar user={user} />
+        <Conditional cond={user.id}>
+          <Navigation>
+            <List>
+              <ListItem button component={Link} to="/">
+                <ListItemText primary={t('label.home')} />
+              </ListItem>
+            </List>
+          </Navigation>
+        </Conditional>
+
+        <Content toolbar>
+          <Routes user={user} onLogin={setUser} />
+        </Content>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export { App };
