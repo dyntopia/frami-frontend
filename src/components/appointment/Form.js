9c59d5d6da911fa0a125e89817fadeb3648c1911
Hans Jerry Illikainen <hji@dyntopia.com>
2019-07-25 17:25:36 +0000

    components: appointment: autocomplete users

--- src/components/appointment/Form.js
+++ src/components/appointment/Form.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { format } from 'date-fns';
+import { useTranslation } from 'react-i18next';
 
-import { Field, Form as MForm } from '../form';
 import { Conditional } from '../Conditional';
-import { Retrieve } from '../Retrieve';
-import { isStaff } from '../../utils';
+import { Field, Form as MForm } from '../form';
+import { Message } from '../Message';
+import { Progress } from '../Progress';
+import { isPatient, isStaff } from '../../utils';
+import { useRequest } from '../../hooks';
 
 const DataForm = ({ aid, data, to, user }) => {
   const [done, setDone] = useState(false);
+  const [state, request] = useRequest();
   const disabled = !isStaff(user);
+
+  if (!state.started) {
+    request('get', '/api/user/');
+  }
 
   /* eslint-disable camelcase */
   return (
@@ -20,18 +28,39 @@ const DataForm = ({ aid, data, to, user }) => {
         staff: user.username,
         ...data,
         start_date: format(
-          data ? data.start_date : Date.now(), 'YYYY-MM-DDTHH:MM'
+          data.start_date || Date.now(), 'YYYY-MM-DDTHH:MM'
         ),
         end_date: format(
-          data ? data.end_date : Date.now(), 'YYYY-MM-DDTHH:MM'
+          data.end_date || Date.now(), 'YYYY-MM-DDTHH:MM'
         ),
       }}
       onDone={() => setDone(true)}
     >
       <Field name="start_date" type="datetime-local" disabled={disabled} />
       <Field name="end_date" type="datetime-local" disabled={disabled} />
-      <Field name="staff" disabled={disabled} />
-      <Field name="patient" disabled={disabled} />
+
+      <Field
+        inputProps={{ list: 'staff' }}
+        name="staff"
+        disabled={disabled}
+      />
+      <datalist id="staff">
+        {(state.data || []).filter(isStaff).map((elt) => (
+          <option key={elt.id} value={elt.username} />
+        ))}
+      </datalist>
+
+      <Field
+        inputProps={{ list: 'patient' }}
+        name="patient"
+        disabled={disabled}
+      />
+      <datalist id="patient">
+        {(state.data || []).filter(isPatient).map((elt) => (
+          <option key={elt.id} value={elt.username} />
+        ))}
+      </datalist>
+
       <Field name="note" disabled={disabled} />
       <Conditional cond={done}>
         <Redirect to={to} />
@@ -42,21 +71,34 @@ const DataForm = ({ aid, data, to, user }) => {
 };
 
 const Form = (props) => {
+  const [message, setMessage] = useState(true);
+  const [state, request] = useRequest();
+  const { t } = useTranslation();
   const { aid } = props;
 
-  return (
-    <>
-      <Conditional cond={aid}>
-        <Retrieve url={`/api/appointment/${aid}/`} method="GET">
-          <DataForm {...props} />
-        </Retrieve>
-      </Conditional>
+  if (aid) {
+    if (!state.started) {
+      request('get', `/api/appointment/${aid}/`);
+      return <Progress />;
+    }
 
-      <Conditional cond={!aid}>
-        <DataForm {...props} />
-      </Conditional>
-    </>
-  );
+    if (state.loading) {
+      return <Progress />;
+    }
+
+    if (state.error) {
+      return (
+        <Message
+          open={message}
+          type="error"
+          text={t('message.apiError')}
+          onClose={() => setMessage(false)}
+        />
+      );
+    }
+  }
+
+  return <DataForm {...props} data={state.data || {}} />;
 };
 
 export { Form };
