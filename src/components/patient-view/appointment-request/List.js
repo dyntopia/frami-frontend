3c199220e7dd2d6eea4a15835fc64ceefc841f35
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-01 01:28:22 +0000

    components: patient-view: clean up appointment-request

--- src/components/patient-view/appointment-request/List.js
+++ src/components/patient-view/appointment-request/List.js
@@ -11,9 +11,7 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 
-import { Conditional } from '../../Conditional';
 import { Retrieve } from '../../Retrieve';
-import { isPatient, isStaff } from '../../../utils';
 
 const useStyles = makeStyles((theme) => ({
   add: {
@@ -28,21 +26,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Cell = ({ pid, page, children, user }) => {
-  const classes = useStyles();
-
-  if (!isStaff(user)) {
-    return (
-      <TableCell>
-        {children}
-      </TableCell>
-    );
-  }
+const Cell = ({ children }) => {
   return (
     <TableCell>
-      <Link className={classes.link} to={`/${page}/${pid}/`}>
-        {children}
-      </Link>
+      {children}
     </TableCell>
   );
 };
@@ -69,7 +56,7 @@ const Row = (props) => {
 };
 
 const List = (props) => {
-  const { page, user } = props;
+  const { page } = props;
   const { t } = useTranslation();
   const classes = useStyles();
 
@@ -94,17 +81,15 @@ const List = (props) => {
         </Table>
       </Grid>
 
-      <Conditional cond={isPatient(user)}>
-        <Fab
-          color="primary"
-          aria-label="Request appointment"
-          className={classes.add}
-          component={Link}
-          to={`/${page}/add/`}
-        >
-          <Add />
-        </Fab>
-      </Conditional>
+      <Fab
+        color="primary"
+        aria-label="Request appointment"
+        className={classes.add}
+        component={Link}
+        to={`/${page}/add/`}
+      >
+        <Add />
+      </Fab>
     </>
   );
 };
