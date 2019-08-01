3da24e81619f86c273486a72e8b6f6148cb3059e
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-01 05:07:10 +0000

    components: patient-view: clean up result

--- src/components/patient-view/result/List.js
+++ src/components/patient-view/result/List.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import Add from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 
 import { Retrieve } from '../../Retrieve';
-import { isStaff } from '../../../utils';
 
-const useStyles = makeStyles((theme) => ({
-  add: {
-    position: 'absolute',
-    right: theme.spacing(2),
-    bottom: theme.spacing(2),
-  },
-  link: {
-    display: 'block',
-    textDecoration: 'none',
-    color: theme.palette.text.primary,
-  },
-}));
-
-const Cell = ({ uid, pid, page, children, user }) => {
-  const classes = useStyles();
-
-  if (!isStaff(user)) {
-    return (
-      <TableCell>
-        {children}
-      </TableCell>
-    );
-  }
+const Cell = ({ children }) => {
   return (
     <TableCell>
-      <Link className={classes.link} to={`/${page}/${uid}/result/${pid}`}>
-        {children}
-      </Link>
+      {children}
     </TableCell>
   );
 };
@@ -59,39 +30,24 @@ const Row = (props) => {
 };
 
 const List = (props) => {
-  const { match: { params: { uid } }, page } = props;
   const { t } = useTranslation();
-  const classes = useStyles();
-  const query = uid ? `?patient=${uid}` : '';
 
   return (
-    <>
-      <Grid container justify="center">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{t('label.kind')}</TableCell>
-              <TableCell>{t('label.result')}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <Retrieve url={`/api/result/${query}`} method="GET">
-              <Row {...props} />
-            </Retrieve>
-          </TableBody>
-        </Table>
-      </Grid>
-
-      <Fab
-        color="primary"
-        aria-label="Add result"
-        className={classes.add}
-        component={Link}
-        to={`/${page}/${uid}/result/add/`}
-      >
-        <Add />
-      </Fab>
-    </>
+    <Grid container justify="center">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('label.kind')}</TableCell>
+            <TableCell>{t('label.result')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <Retrieve url="/api/result/" method="GET">
+            <Row {...props} />
+          </Retrieve>
+        </TableBody>
+      </Table>
+    </Grid>
   );
 };
 
--- src/components/patient-view/result/View.js
+++ src/components/patient-view/result/View.js
@@ -1,20 +1,8 @@
 import React from 'react';
 
-import { Form } from './Form';
 import { List } from './List';
 
 const View = (props) => {
-  const { match: { params: { add, uid, mid } }, page } = props;
-  const to = `/${page}/${uid}/result/`;
-
-  if (mid) {
-    return <Form {...props} rid={mid} to={to} />;
-  }
-
-  if (add) {
-    return <Form {...props} to={to} />;
-  }
-
   return <List {...props} />;
 };
 
